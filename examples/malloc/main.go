7caafb55f3098d86d441c6d3aa4e3e841550f698
fmeng <fmeng@ebay.com>
2021-08-16 15:07:16 +0800

    add memoryconsumption

--- examples/malloc/main.go
+++ examples/malloc/main.go
@@ -12,7 +12,7 @@ func main() {
 	// only loop 10 times to avoid exhausting the host memory
 	for i := 1; i <= 10; i++ {
 		// allocMemory malloc and memset 100Mb per time
-		fmt.Printf("Allocating %dMb memory\n", i*100)
+		fmt.Printf("Allocating %dMb memory, raw memory is %d\n", i*100, i*100*1024*1025)
 		C.allocMemory()
 		time.Sleep(time.Minute)
 	}
