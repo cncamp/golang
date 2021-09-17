c97126f79949a4079518104afd7cf4ac4e5abba8
Jesse <jesse@MacBook-Pro.local>
2021-09-17 22:59:30 +0800

    point parameter

--- examples/interface/main.go
+++ examples/interface/main.go
@@ -10,8 +10,17 @@ type Human struct {
 	firstName, lastName string
 }
 
+type Plane struct {
+	vendor string
+	model  string
+}
+
 func (h *Human) getName() string {
 	return h.firstName + "," + h.lastName
+}
+
+func (p Plane) getName() string {
+	return fmt.Sprintf("vendor: %s, model: %s", p.vendor, p.model)
 }
 
 type Car struct {
@@ -35,4 +44,8 @@ func main() {
 	for _, f := range interfaces {
 		fmt.Println(f.getName())
 	}
+	p := Plane{}
+	p.vendor = "testVendor"
+	p.model = "testModel"
+	fmt.Println(p.getName())
 }
