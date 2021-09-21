043358e8cf874b3cd1879ff4b4762cffa7aace9a
Jesse <jesse@MacBook-Pro.local>
2021-09-21 15:56:36 +0800

    add module folders

---
+++ examples/module2/mutex/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	loopFunc()
+	time.Sleep(time.Second)
+}
+
+func loopFunc() {
+	lock := sync.Mutex{}
+	for i := 0; i < 3; i++ {
+		// go func(i int) {
+		lock.Lock()
+		defer lock.Unlock()
+		fmt.Println("loopFunc:", i)
+		// }(i)
+	}
+}
