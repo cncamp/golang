37dfb08d4e4147f158458eb271274a8878361c6e
郑海成 <zhenghc@gih.cn>
2022-04-26 15:58:18 +0800

    TestIncrease should test func increase(int, int) int in main.go

--- examples/module1/callbacks/main_test.go
+++ examples/module1/callbacks/main_test.go
@@ -6,11 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func add(a, b int) int {
-	return a + b
-}
 func TestIncrease(t *testing.T) {
 	t.Log("Start testing")
-	result := add(1, 2)
+	result := increase(1, 2)
 	assert.Equal(t, result, 3)
 }
