8c587d726a68cf83fa967488f9fbe269ea2194c3
郑海成 <zhenghc@gih.cn>
2022-04-26 16:25:51 +0800

    TestIncrease should test func increase(int, int) int in main.go

--- examples/module1/callbacks/main.go
+++ examples/module1/callbacks/main.go
@@ -1,20 +1,20 @@
 package main
 
 func main() {
-	var a *int
-	*a += 1
-	// DoOperation(1, increase)
+	DoOperation(1, increase)
 	DoOperation(1, decrease)
 }
 
 func increase(a, b int) int {
+	println("increase result is:", a+b)
 	return a + b
 }
 
-func DoOperation(y int, f func(int, int)) {
+func DoOperation(y int, f func(int, int) int) {
 	f(y, 1)
 }
 
-func decrease(a, b int) {
+func decrease(a, b int) int {
 	println("decrease result is:", a-b)
+	return a - b
 }
